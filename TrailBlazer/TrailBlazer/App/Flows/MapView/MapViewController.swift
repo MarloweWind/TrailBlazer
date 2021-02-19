6400eb3c0eb4d7547b6a920f75d4148430f49f1a
Alexey Malkov <marlowe.wind@gmail.com>
2021-02-19 18:34:23 +0300

    fix

--- TrailBlazer/TrailBlazer/App/Flows/MapView/MapViewController.swift
+++ TrailBlazer/TrailBlazer/App/Flows/MapView/MapViewController.swift
@@ -44,3 +44,18 @@ class MapViewController: UIViewController {
     }
     
 }
+
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            mapView.camera = GMSCameraPosition(target: location.coordinate, zoom: 15, bearing: 0, viewingAngle: 0)
+            let marker = GMSMarker(position: location.coordinate)
+            marker.map = mapView
+        }
+    }
+    // TODO: fix it
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+
+}
